e9a74c59705b73aa847d490132122636838eb021
gbwiersum <gbwiersum@gmail.com>
2020-12-08 17:02:49 -0600

    Work on conversion of minstrel images to tfrecord format

--- ImageRecognition/Initial_training/initial_image_train.py
+++ ImageRecognition/Initial_training/initial_image_train.py
@@ -1,58 +1,16 @@
 # Much of the code used here is based on: https://keras.io/examples/keras_recipes/tfrecord/
 import tensorflow as tf
-import os
-from keras.models import Sequential
-from keras.layers import Conv2D, Conv3D, MaxPooling2D, Flatten, Dense, Dropout
-from ImageRecognition.utils.get_dataset import get_dataset
-from ImageRecognition.utils.inception_v4 import create_model
+from ImageRecognition.utils.DataGenerator import get_dataset
+from ImageRecognition.utils.Inceptionv4 import create_model
+import glob
 
-tf.compat.v1.enable_eager_execution()
-
-
-def get_file_list(directory):
-    file_list = []
-    # Loading training dataset:
-    for absolute_path in list(os.walk(directory))[0][2]:
-        file_list.append(directory + absolute_path)
-    return file_list
-
-
-# Features as provided by MARCO
-feature_description = {
-    "image/height": tf.io.VarLenFeature(tf.int64),  # image height in pixels
-    "image/width": tf.io.VarLenFeature(tf.int64),  # image width in pixels
-    "image/colorspace": tf.io.VarLenFeature(tf.string),  # specifying the colorspace, always 'RGB'
-    "image/channels": tf.io.VarLenFeature(tf.int64),  # specifying the number of channels, always 3
-    "image/class/label": tf.io.VarLenFeature(tf.int64),  # specifying the index in a normalized classification layer
-    "image/class/raw": tf.io.VarLenFeature(tf.int64),  # specifying the index in the raw (original) classification layer
-    "image/class/source": tf.io.VarLenFeature(tf.int64),  # specifying the index of the source (creator of the image)
-    "image/class/text": tf.io.VarLenFeature(tf.string),  # specifying the human-readable version of the normalized label
-    "image/format": tf.io.VarLenFeature(tf.string),  # specifying the format, always 'JPEG'
-    "image/filename": tf.io.VarLenFeature(tf.string),  # containing the basename of the image file
-    "image/id": tf.io.VarLenFeature(tf.int64),  # specifying the unique id for the image
-    "image/encoded": tf.io.VarLenFeature(tf.string),  # containing JPEG encoded image in RGB colorspace
-}
-
-
-def get_model():
-    model = create_model(num_classes=4)
-    model.compile(optimizer='adam', loss=tf.keras.losses.categorical_crossentropy,
-                  metrics=tf.keras.metrics.categorical_accuracy)
-    return model
-
-
-train_dir = './train-jpg/'
-test_dir = './test-jpg/'
-valid_dir = './test-jpg/'
-
-train_list = get_file_list(train_dir)
-test_list = get_file_list(test_dir)
-valid_list = get_file_list(valid_dir)
+train_list = glob.glob('../train-jpg/*')[0:2]
+test_list = glob.glob('../test-jpg/*')[0:1]
+valid_list = test_list
 
 # Tuning and training params:
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 10
-IMAGE_SIZE = [512, 512, 3]
+BATCH_SIZE = 1
+IMAGE_SIZE = [299, 299, 3]
 keras_batch_size = BATCH_SIZE
 learning_rate = 0.095
 epochs = 1
@@ -60,9 +18,13 @@ epochs = 1
 print("Train Files: ", len(train_list))
 print("Test Files: ", len(test_list))
 
-train_dataset = get_dataset(train_list, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE)
-valid_dataset = get_dataset(test_list, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE)
-test_dataset = get_dataset(test_list, labeled=False, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE)
-model = get_model()
+train_dataset = get_dataset(train_list, batch_size=BATCH_SIZE, im_size=IMAGE_SIZE)
+valid_dataset = get_dataset(test_list, batch_size=BATCH_SIZE, im_size=IMAGE_SIZE)
+test_dataset = get_dataset(test_list, labeled=False, batch_size=BATCH_SIZE, im_size=IMAGE_SIZE)
+
+model = create_model(num_classes=1)
+model.compile(optimizer='adam', loss=tf.keras.losses.binary_crossentropy,
+              metrics=tf.keras.metrics.binary_accuracy)
+
 model.fit(train_dataset, validation_data=valid_dataset, batch_size=keras_batch_size, epochs=epochs)
 model.save('../Models/0000csgid-image-rec.h5')
